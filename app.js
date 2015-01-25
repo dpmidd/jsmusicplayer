336e03c4ff4533eb7b452c0b20ee62e183a5786d
Dmidd87 <dmidd87@gmail.com>
2015-01-24 17:56:16 -0700

    Finished product

--- app.js
+++ app.js
@@ -1,24 +1,15 @@
 "use strict";
 
 $('.container').on('click', '.fa-play', function() {
-
-  // Class .fa that is not the thing that was clicked - pause removed and play added
   $('.fa').not(this).removeClass('fa-stop').addClass('fa-play');
-
-  // For the thing that was clicked - play is removed and pause is added
   $(this).removeClass('fa-play').addClass('fa-stop');
-
-  // For the thing that was clicked, grab the value of the data title attribute and use it in h1 header
   var info = $(this).data('title');
   $("h1").html("Now Playing: " + info);
-
-  // For the thing that was clicked, grab the value of the data id attribute and use it to get the audio by id
   var id = $(this).data('id');
   var sound = document.getElementById(id);
   sound.load();
   sound.play();
 
-  // For everything that did not have a play event, pause it
   document.addEventListener('play', function(current){
     var otherAudio = document.getElementsByTagName('audio');
     for(var i = 0, length = otherAudio.length; i < length; i++){
@@ -27,30 +18,18 @@ $('.container').on('click', '.fa-play', function() {
       }
     }
   }, true);
-
 });
 
 $('.container').on('click', '.fa-stop', function(){
-
   $(this).removeClass('fa-stop').addClass('fa-play');
-
-  var id = $(this).data('id');
-
+    var id = $(this).data('id');
   $("h1").html("Select a Song!");
-
-  var sound = document.getElementById(id);
-
+    var sound = document.getElementById(id);
   sound.pause();
-
 });
 
-
 jQuery.getJSON('data.json', function(tracks) {
-
   var $tracksTemplate = $('#tracksTemplate').html();
-
   var newHTML = Mustache.to_html($tracksTemplate, tracks);
-
   $('.tracks').html(newHTML);
-
 });
